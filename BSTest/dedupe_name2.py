dc381af06509f79786ac4d5a5a68c29f03da17d6
bisuser <intelvision.sc@outlook.com>
2018-11-19 16:10:47 +0400

    data-bis

--- BSTest/dedupe_name2.py
+++ BSTest/dedupe_name2.py
@@ -10,6 +10,10 @@ import itertools
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import os
+import time
+
+start_time = time.time()
+print("Starts:" + time.asctime(time.localtime(start_time)))
 
 config = configparser.ConfigParser()
 config.read('dbconfig.ini')
@@ -36,6 +40,8 @@ header2 = ["kod1", "firm1", "kod2", "firm2", "score"]
 file1 = 'C:\_out\custname1.csv'
 file2 = 'C:\_out\custname2.csv'
 
+print("Time Elapsed 1 :" + str(time.time() - start_time))
+
 with open(file1, 'w', newline='', encoding="utf-8") as outfile:
     writer = csv.writer(outfile)
 
@@ -50,6 +56,8 @@ with open(file1, 'w', newline='', encoding="utf-8") as outfile:
             # print(temp)
             writer.writerow(temp)
 
+print("Time Elapsed 2 :" + str(time.time() - start_time))
+
 with open(file2, 'w', newline='', encoding="utf-8") as outfile2:
     writer2 = csv.writer(outfile2)
 
@@ -62,7 +70,9 @@ with open(file2, 'w', newline='', encoding="utf-8") as outfile2:
     # data = []
     # dfr = pd.DataFrame()
     next(f, None)
+    i = 0
     for line in f:
+        i = i + 1
         data_line = line.rstrip().split(',')
         ele1 = data_line[0].split('||')
         ele2 = data_line[1].split('||')
@@ -82,8 +92,15 @@ with open(file2, 'w', newline='', encoding="utf-8") as outfile2:
             temp2 = (kod1, firm1, kod2, firm2, score)
             writer2.writerow(temp2)
 
+        if i % 100000 == 0:
+            print("Time Elapsed:" + str(i) + " - " + str(time.time() - start_time))
+
 f.close()
 
 # dfr2.to_csv('C:\_out\custname2.csv', index=False, encoding='utf-8')
 
 os.remove(file1)
+
+print("Time Elapsed 3 :" + str(time.time() - start_time))
+
+print("Ends:" + time.asctime(time.localtime(start_time)))
