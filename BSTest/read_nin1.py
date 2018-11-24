9a505fe70cd14f91ad82eef2794b99901df8f465
bisuser <intelvision.sc@outlook.com>
2018-11-24 14:17:40 +0400

    data-bis

--- BSTest/read_nin1.py
+++ BSTest/read_nin1.py
@@ -16,15 +16,14 @@ from tqdm import tqdm
 start_time = time.time()
 print("Starts:" + time.asctime(time.localtime(start_time)))
 
-
-header = ["kod1||firm1", "kod2||firm2"]
-header2 = ["kod1", "firm1", "kod2", "firm2", "score"]
-header3 = ["kod1", "firm1", "kod2", "firm2", "score_r", "score_pr", "score_tsor", "score_tser", "score_ptsor",
+header = ["kod1||firm1||danno1", "kod2||firm2||danno2"]
+header2 = ["kod1", "firm1", "danno1", "kod2", "firm2", "danno2", "score"]
+header3 = ["kod1", "firm1", "danno1", "kod2", "firm2", "danno2", "score_r", "score_pr", "score_tsor", "score_tser", "score_ptsor",
            "score_ptser", "score_qr", "score_uqr", "score_wr", "score_uwr"]
 
-file1 = 'C:\_out\custname1.csv'
-file2 = 'C:\_out\custname2.csv'
-file3 = 'C:\_out\custname3.csv'
+file1 = 'C:\_out\custnin1.csv'
+file2 = 'C:\_out\custnin2.csv'
+file3 = 'C:\_out\custnin3.csv'
 
 print("Time Elapsed 1 :" + str(time.time() - start_time))
 
@@ -63,46 +62,48 @@ with open(file3, 'w', newline='', encoding="utf-8") as outfile3:
     i = 0
     for line in f:
         i = i + 1
-
         data_line = line.rstrip().split(',')
         ele1 = data_line[0].split('||')
         ele2 = data_line[1].split('||')
         kod1 = ele1[0]
         firm1 = ele1[1]
+        nin1 = ele1[2]
         kod2 = ele2[0]
         firm2 = ele2[1]
-        # score = fuzz.token_set_ratio(firm1, firm2)
+        nin2 = ele2[2]
 
-        score_r = fuzz.ratio(firm1, firm2)
-        score_pr = fuzz.partial_ratio(firm1, firm2)
-        score_tsor = fuzz.token_sort_ratio(firm1, firm2)
-        score_tser = fuzz.token_set_ratio(firm1, firm2)
-        score_ptsor = fuzz.partial_token_sort_ratio(firm1, firm2)
-        score_ptser = fuzz.partial_token_set_ratio(firm1, firm2)
-        score_qr = fuzz.QRatio(firm1, firm2)
-        score_uqr = fuzz.UQRatio(firm1, firm2)
-        score_wr = fuzz.WRatio(firm1, firm2)
-        score_uwr = fuzz.UWRatio(firm1, firm2)
+        if nin1 != '986-1145-1-0-94' and nin1 != '005-0521-7-1-23' and nin1 != '986-0567-1-1-10':
+            score_r = fuzz.ratio(nin1, nin2)
+            score_pr = fuzz.partial_ratio(nin1, nin2)
+            score_tsor = fuzz.token_sort_ratio(nin1, nin2)
+            score_tser = fuzz.token_set_ratio(nin1, nin2)
+            score_ptsor = fuzz.partial_token_sort_ratio(nin1, nin2)
+            score_ptser = fuzz.partial_token_set_ratio(nin1, nin2)
+            score_qr = fuzz.QRatio(nin1, nin2)
+            score_uqr = fuzz.UQRatio(nin1, nin2)
+            score_wr = fuzz.WRatio(nin1, nin2)
+            score_uwr = fuzz.UWRatio(nin1, nin2)
 
-        # print('kod1:' + kod1)
-        # print('firm1:' + firm1)
-        # print('kod2:' + kod2)
-        # print('firm2:' + firm2)
-        # print('score:' + str(score))
+            # print('kod1:' + kod1)
+            # print('firm1:' + firm1)
+            # print('kod2:' + kod2)
+            # print('firm2:' + firm2)
+            # print('score:' + str(score))
 
-        # if score_r > 90 or score_pr > 90 or score_tsor > 90 or score_tser > 90 or score_ptsor > 90 or score_ptser > 90 \
-        #         or score_qr > 90 or score_uqr > 90 or score_wr > 90 or score_uwr > 90:
+            # if score_r > 90 or score_pr > 90 or score_tsor > 90 or score_tser > 90 or score_ptsor > 90 or score_ptser > 90 \
+            #         or score_qr > 90 or score_uqr > 90 or score_wr > 90 or score_uwr > 90:
 
-        if score_tser > 90:
-            temp3 = (
-                kod1, firm1, kod2, firm2, score_r, score_pr, score_tsor, score_tser, score_ptsor, score_ptser, score_qr,
-                score_uqr, score_wr, score_uwr)
-            writer3.writerow(temp3)
+            if score_tser > 90:
+                temp3 = (
+                    kod1, firm1, nin1, kod2, firm2, nin2, score_r, score_pr, score_tsor, score_tser, score_ptsor,
+                    score_ptser, score_qr,
+                    score_uqr, score_wr, score_uwr)
+                writer3.writerow(temp3)
 
         if i % 10000000 == 0:
             print("Time Elapsed:" + str(i) + " - " + str(time.time() - start_time))
 
-        # if i > 1000000:
+        # if i > 10000000:
         #     print("Breaking loop: " + str(i) + " - " + str(time.time() - start_time))
         #     outfile3.close()
         #     # fragile.Break
